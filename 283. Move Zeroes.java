378deb778e85abbe1bc3c8dbc1ed195807a8c31d
haoning66 <46359666+haoning66@users.noreply.github.com>
2020-01-16 14:56:49 -0500

    Add files via upload

---
+++ 283. Move Zeroes.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0)
+                nums[index++] = nums[i];
+        }
+
+        for (; index < nums.length; index++) {
+            nums[index] = 0;
+        }
+    }
+}
