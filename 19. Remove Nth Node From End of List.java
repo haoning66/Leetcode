bbee3b5e0f7d2daf88b21e5f4da8acaeccee12f6
haoning66 <46359666+haoning66@users.noreply.github.com>
2022-04-12 10:57:01 -0400

    Add files via upload

--- 19. Remove Nth Node From End of List.java
+++ 19. Remove Nth Node From End of List.java
@@ -1,29 +1,32 @@
-import javafx.util.Pair;
-
-import java.util.*;
-
-
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode dummy = new ListNode(0);
+        ListNode dummy = new ListNode(-1);
         dummy.next = head;
-        ListNode first = dummy;
-        ListNode second = dummy;
-
-        for (int i = 0; i < n; i++) {
-            first = first.next;
+        ListNode fast = dummy;
+        
+        while (n > 0) {
+            fast = fast.next;
+            n--;
         }
-
-        while (first.next != null) {
-            second = second.next;
-            first = first.next;
+        
+        ListNode slow = dummy;
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
         }
-        second.next = second.next.next;
+        
+        slow.next = slow.next.next;
+        
         return dummy.next;
     }
-
-    public static void main(String[] args) {
-
-    }
-
 }
