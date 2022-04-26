934e43ab7d8d3080e844e7a8ec0b482de8c7bf76
haoning66 <46359666+haoning66@users.noreply.github.com>
2022-04-25 22:25:05 -0400

    Add files via upload

--- 160. Intersection of Two Linked Lists.java
+++ 160. Intersection of Two Linked Lists.java
@@ -1,35 +1,62 @@
-import java.util.*;
-
-class Solution {
-    //let a be headA and b be headB, move to the next element during each iteration, when a reach the end of A let a be headB
-    //and when b reach the end of B, let b be headA, the distance between a and b is caused by the length of A and B before the intersection
-    //if switch their route, one pointer will catch up the other pointer, and the only possibility is that they meet at the intersection
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        if (headA == null || headB == null)
+        if(headA ==  null || headB == null)
             return null;
-        ListNode a = headA;
-        ListNode b = headB;
-        ListNode lastA = null;
-        ListNode lastB = null;
-        while (a != b) {
-            if (lastA != null && lastB != null) {
-                if (lastA != lastB)
-                    return null;
-            }
-            if (a.next == null) {
-                lastA = a;
-                a = headB;
-            }
-            else
-                a = a.next;
-            if (b.next == null) {
-                lastB = b;
-                b = headA;
-            }
-            else
-                b = b.next;
-
+        
+        ListNode la = headA;
+        ListNode lb = headB;
+        
+        int a = 0;
+        int b = 0;
+        
+        while(la != null) {
+            a++;
+            la = la.next;
         }
-        return a;
+        
+        while(lb != null) {
+            b++;
+            lb = lb.next;
+        }
+        
+        la = headA;
+        lb = headB;
+        
+        if(a > b) {
+            a = a - b;
+            
+            while(a > 0) {
+                la = la.next;
+                a--;
+            }
+        } else if(b > a) {
+            b = b - a;
+
+            while(b > 0) {
+                lb = lb.next;
+                b--;
+            }
+        }
+        
+        while(la != lb && la != null && lb != null) {
+            la = la.next;
+            lb = lb.next;
+        }
+        
+        if(la == lb)
+            return la;
+        else
+            return null;
     }
 }
