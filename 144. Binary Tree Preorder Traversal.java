1dd4271f10c6a80cdccb872df5247f5ed4614d2a
haoning66 <46359666+haoning66@users.noreply.github.com>
2022-05-03 20:00:25 -0400

    Add files via upload

---
+++ 144. Binary Tree Preorder Traversal.java
@@ -0,0 +1,39 @@
+/**                  THE BEST SOLUTION
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        
+        if(root == null)
+            return list;
+        
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        
+        while(!stack.isEmpty()) {
+            root = stack.pop();
+            list.add(root.val);
+            
+            if(root.right != null)
+                stack.push(root.right);
+            
+            if(root.left != null)
+                stack.push(root.left);
+        }
+        
+        return list;
+    }
+}
